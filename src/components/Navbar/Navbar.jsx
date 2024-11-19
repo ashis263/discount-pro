312c285fd03a2fd6a88d867a51341b5967d85165
Ashis Kumar Joy <ashis263@gmail.com>
2024-11-20 00:37:33 +0600

    added navbar contents for logged in and logged out users

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,14 @@ import { FaLaptopCode } from "react-icons/fa";
 import { MdOutlineExplore } from "react-icons/md";
 import { BsPersonVcard } from "react-icons/bs";
 import './navbar.css'
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { HiUser } from "react-icons/hi2";
+import { HiOutlineLogout } from "react-icons/hi";
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
+    const { user } = useContext(AuthContext);
     const navLinks = <>
         <NavLink to='/' className="flex items-center gap-1"><TbHome />
             Home</NavLink>
@@ -14,7 +20,7 @@ const Navbar = () => {
         <NavLink to='/about' className="flex items-center gap-1"><FaLaptopCode className="text-xs" />About Dev</NavLink>
     </>
     return (
-        <div className="navbar w-11/12 mx-auto">
+        <div className="navbar w-11/12 mx-auto py-1 sm:py-3">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -37,7 +43,10 @@ const Navbar = () => {
                         {navLinks}
                     </ul>
                 </div>
-                <Link to="/" className="btn btn-ghost font-oldenburg text-primary text-x sm:text-2xl p-0 font-extrabold">Discount Pro</Link>
+                <div className="flex items-center gap-2">
+                    <img className="w-8 sm:w-10" src={logo} alt="logo" />
+                    <Link to="/" className="hidden sm:flex btn btn-ghost font-oldenburg text-primary text-2xl sm:text-3xl p-0 font-extrabold hover:bg-transparent">Discount Pro</Link>
+                </div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 space-x-5">
@@ -45,7 +54,23 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-
+                {
+                    user
+                        ?
+                        <div className="space-x-2 flex items-center">
+                            <div>
+                                <img className="w-6 sm:w-7 rounded-xl" src={user.photoURL} alt="" />
+                            </div>
+                            <p className="text-[10px] hidden sm:flex p-1 py-2 border rounded-md">{user.email}</p>
+                            <button className="btn btn-outline border-gray-200 btn-xs sm:btn-sm  hover:bg-transparent text-primary hover:text-primary">Logout<HiOutlineLogout /></button>
+                        </div>
+                        :
+                        <div className="space-x-2 flex items-center text-primary">
+                            <Link to="/login" className="btn hover:bg-transparent btn-xs sm:btn-sm btn-ghost"><HiUser />Login</Link >
+                            <p>|</p>
+                            <Link to="/register" className="btn hover:bg-transparent btn-xs sm:btn-sm btn-ghost"><HiUser />Registration</Link >
+                        </div>
+                }
             </div>
         </div>
     );
