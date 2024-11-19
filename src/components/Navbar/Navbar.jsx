a4613f0ad438446aca363599077b1279279eba0f
Ashis Kumar Joy <ashis263@gmail.com>
2024-11-20 00:41:31 +0600

    added auth observer, logout feature, loading state to show loader before user loading

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { FaLaptopCode } from "react-icons/fa";
 import { MdOutlineExplore } from "react-icons/md";
 import { BsPersonVcard } from "react-icons/bs";
 import './navbar.css'
+import { signOut } from "firebase/auth";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import { HiUser } from "react-icons/hi2";
@@ -11,7 +12,7 @@ import { HiOutlineLogout } from "react-icons/hi";
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
-    const { user } = useContext(AuthContext);
+    const { user, auth, isLoading } = useContext(AuthContext);
     const navLinks = <>
         <NavLink to='/' className="flex items-center gap-1"><TbHome />
             Home</NavLink>
@@ -19,6 +20,14 @@ const Navbar = () => {
         <NavLink to='/profile' className="flex items-center gap-1"><BsPersonVcard className="text-xs" />My Profile</NavLink>
         <NavLink to='/about' className="flex items-center gap-1"><FaLaptopCode className="text-xs" />About Dev</NavLink>
     </>
+    const handleLogOut = () => {
+        signOut(auth);
+    }
+    if (isLoading) {
+        return <div className="flex justify-center items-center h-svh">
+            <span className="loading loading-bars flex justify-center loading-lg"></span>
+        </div>
+    }
     return (
         <div className="navbar w-11/12 mx-auto py-1 sm:py-3">
             <div className="navbar-start">
@@ -62,7 +71,7 @@ const Navbar = () => {
                                 <img className="w-6 sm:w-7 rounded-xl" src={user.photoURL} alt="" />
                             </div>
                             <p className="text-[10px] hidden sm:flex p-1 py-2 border rounded-md">{user.email}</p>
-                            <button className="btn btn-outline border-gray-200 btn-xs sm:btn-sm  hover:bg-transparent text-primary hover:text-primary">Logout<HiOutlineLogout /></button>
+                            <button onClick={handleLogOut} className="btn btn-outline border-gray-200 btn-xs sm:btn-sm  hover:bg-transparent text-primary hover:text-primary">Logout<HiOutlineLogout /></button>
                         </div>
                         :
                         <div className="space-x-2 flex items-center text-primary">
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import 'react-toastify/dist/ReactToastify.css';
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -8,16 +8,27 @@ export const AuthContext = createContext();
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
     const auth = getAuth(app)
     const provider = new GoogleAuthProvider();
     const handleGoogleSignin = () => {
+        setIsLoading(false);
         return signInWithPopup(auth, provider)
     }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setIsLoading(false);
+        });
+        return () => unsubscribe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     const authData = {
         handleGoogleSignin,
         user,
         setUser,
         auth,
+        isLoading
     }
     return (
         <AuthContext.Provider value={authData}>
